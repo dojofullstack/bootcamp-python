437c283123f40b966c7dda74110fa056769961f8
Dojopy <lifehack.py@gmail.com>
2022-04-11 21:12:12 -0500

    sources python data types

---
+++ RaulArispe.py
@@ -0,0 +1,77 @@
+
+#Este es un comentario en python
+
+print('Hola mundo')
+
+"""Esto es un comentario 
+de varias lineas
+"""
+
+"""Tipos de Datos"""
+
+# Tipo de dato String: "hola mundo"
+# Tipo de dato Numeral (Int o float): 10, 10.5
+# Tipo de dato Array o listas: ["python", "Raul", "Javascrip"]
+# Tipo de dato Booleano: True / False
+# Tipo de dato Diccionario: {'usuario': 'Raul', 'email': 'raularispe@hotmail.com'}
+# Tipo de dato conjunto o set:  {'c++', 'java'}
+# Tipo de dato Tupla o tuple:  ('pablo', 'rosa')
+
+
+
+""" Tipo de dato Sring """
+mystack_favorito = "python"
+
+print(type(mystack_favorito))
+
+print(dir(mystack_favorito))
+
+print(mystack_favorito.upper())
+print(mystack_favorito.lower())
+print(mystack_favorito.replace('py', '00') )
+print(mystack_favorito.find('xyz'))
+print(mystack_favorito.isnumeric())
+
+conversion_string = str(12)
+
+print(type(conversion_string))
+
+
+
+
+# esto es un comentario en Python
+print('hello world')
+
+""" esto es un
+comentario de multiples lineas
+"""
+
+"""  Tipos de datos en Python """
+
+
+# Tipo de dato String: 'hola mundo'  str()
+# Tipo de dato Numeral (int o float):    10, 10.5  int()  float()
+# Tipo de dato array o listas:  ['python', 'javascript']  list()
+# Tipo de dato Booleano: True / False  bool()
+# Tipo de dato Diccionario:  {'usuario': 'henry', 'email': 'hola@dojopy.com}  dict()
+# Tipo de dato conjunto o set:  {'c++', 'java'} set()
+# Tipo de dato Tupla o tuple:  ('pablo', 'rosa')  tuple()
+
+
+
+""" Tipo de dato Sring """
+mystack_favorito = "python python"
+
+print(type(mystack_favorito))
+
+print(dir(mystack_favorito))
+
+print(mystack_favorito.upper())
+print(mystack_favorito.lower())
+print(mystack_favorito.replace('py', '00') )
+print(mystack_favorito.find('xyz'))
+print(mystack_favorito.isnumeric())
+
+conversion_string = str(12)
+
+print(type(conversion_string))
---
+++ moisescanales.py
@@ -0,0 +1,76 @@
+#Comentario para probar
+
+#Imprimr mensaje
+print('hola mundo')
+
+"""Esto es un comentario de
+multiples 
+lineas
+"""
+
+"""Tipos de datos en python"""
+
+#Tipo de dato String 'hola mundo' str()
+#tipo de dato Numeral: (int o float) 10, 10.5 int() float()
+#Tipo de dato array o listas: ['python', 'javascript'] list()
+#Tipo de dato booleano: True/False bool()
+#Tipo de dato Diccionario: {'usuario': 'henry', 'email':'hola@dojopy.com'} dict()
+#Tipo de dato conjunto o set :{'c++', 'java'}
+#Tipo de dato tupla o tuple: ('pablo', 'rosa')
+
+"""Tipo de dato string"""
+
+mystack='python'
+
+type(mystack) 
+
+#Imprimr uppercase
+print(mystack.upper())
+print(mystack.lower())
+print(mystack.replace('py', '00'))
+print(mystack.find(('thon')))
+print(mystack.isnumeric())
+
+conversion_string=str(12)
+print(type(conversion_string))
+
+
+
+
+
+# esto es un comentario en Python
+print('hello world')
+
+""" esto es un
+comentario de multiples lineas
+"""
+
+"""  Tipos de datos en Python """
+
+
+# Tipo de dato String: 'hola mundo'  str()
+# Tipo de dato Numeral (int o float):    10, 10.5  int()  float()
+# Tipo de dato array o listas:  ['python', 'javascript']  list()
+# Tipo de dato Booleano: True / False  bool()
+# Tipo de dato Diccionario:  {'usuario': 'henry', 'email': 'hola@dojopy.com}  dict()
+# Tipo de dato conjunto o set:  {'c++', 'java'} set()
+# Tipo de dato Tupla o tuple:  ('pablo', 'rosa')  tuple()
+
+
+
+""" Tipo de dato Sring """
+mystack_favorito = "python python"
+
+print(type(mystack_favorito))
+
+print(dir(mystack_favorito))
+
+print(mystack_favorito.upper())
+print(mystack_favorito.lower())
+print(mystack_favorito.replace('py', '00') )
+print(mystack_favorito.find('xyz'))
+print(mystack_favorito.isnumeric())
+
+conversion_string = str(12)
+
+print(type(conversion_string))
